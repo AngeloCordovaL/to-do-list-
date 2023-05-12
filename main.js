c8eb2db6ed4b58e4279876bc78a1003fd8f139cb
AngeloCordovaLe <angeloleyva45@gmail.com>
2023-05-11 21:47:17 -0500

    Proyecto Html - Estilo basico3

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("hola mundo");
+console.log("Es hora de programar");
+
+
+function apretar() {
+    console.log(document.getElementById("titulo").innerHTML);
+    document.getElementById("Lista de Tareas").innerHTML = "bienvenido" ;
+} 
